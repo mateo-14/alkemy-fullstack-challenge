e5d274b573dedb045df1398aa907f404656484b4
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-10-16 20:37:00 -0300

    Create pages AuthGuard

---
+++ frontend/src/guards/AuthGuard.js
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import useAuth from '../hooks/useAuth';
+
+export default function AuthGuard({ children }) {
+  const { user, isReady } = useAuth();
+  const { push } = useRouter();
+
+  useEffect(() => {
+    if (isReady && !user) {
+      push({ pathname: '/login' });
+    }
+  }, [user, isReady, push]);
+  return <>{children}</>;
+}
