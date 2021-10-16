87014c08fe6e1d56bba90537d31aaf23d7ad39e1
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-10-16 20:38:55 -0300

    Add button disabled state

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,6 +1,12 @@
-export default function Button({ children, className, onClick }) {
+export default function Button({ children, className, onClick, disabled }) {
   return (
-    <button className={`bg-indigo-500 border-rounded text-white font-medium py-2 ${className || ''}`} onClick={onClick}>
+    <button
+      className={`bg-indigo-500 border-rounded text-white font-medium py-2 disabled:bg-indigo-400 disabled:cursor-default ${
+        className || ''
+      }`}
+      onClick={onClick}
+      disabled={disabled}
+    >
       {children}
     </button>
   );
