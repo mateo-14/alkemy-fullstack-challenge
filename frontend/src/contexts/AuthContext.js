f389e282f1e4d638fa0b7ce37529a09c6d5bd34b
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-10-16 20:42:12 -0300

    Add auth loading and push after auth

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 
 export const AuthContext = React.createContext();
@@ -7,13 +8,14 @@ const api = process.env.NEXT_PUBLIC_API_URL;
 export function AuthProvider({ children }) {
   const [isReady, setIsReady] = useState(false);
   const [user, setUser] = useState();
+  const { push } = useRouter();
 
   const auth = async (postData, endpoint) => {
     if (!isReady || user) return;
-
     const { data } = await axios.post(`${api}/auth/${endpoint}`, postData);
     setUser(data);
     localStorage.setItem('token', data.token);
+    push('/');
   };
 
   const login = (data) => auth(data, 'login');
@@ -29,6 +31,7 @@ export function AuthProvider({ children }) {
         .then(({ data }) => {
           localStorage.setItem('token', data.token);
           setUser(data);
+          push('/');
         })
         .catch((err) => {
           if (err.response.status === 401) localStorage.removeItem('token');
@@ -37,5 +40,27 @@ export function AuthProvider({ children }) {
     }
   }, [isReady]);
 
-  return <AuthContext.Provider value={{ isReady, login, register }}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ isReady, login, register, user }}>
+      {!isReady ? (
+        <div className="min-h-screen flex justify-center items-center">
+          <svg
+            className="animate-spin h-20 w-20 text-indigo-600"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </div>
+      ) : (
+        children
+      )}
+    </AuthContext.Provider>
+  );
 }
