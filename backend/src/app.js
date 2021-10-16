ec21bc96b893ca018caced3213ed1e165dd6c412
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-10-16 19:55:10 -0300

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const authRoutes = require('./routes/auth.routes');
 const transactionsRoutes = require('./routes/transactions.routes');
+const cors = require('cors');
 
 //Middlewares
 app.use(express.json());
+app.use(cors());
 
 //Routes
 app.use('/auth', authRoutes);
