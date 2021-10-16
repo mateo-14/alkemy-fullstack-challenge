fcb14fe60d30be7aad7f7927b6a516b0a36a9b15
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-10-16 20:37:30 -0300

    add sequelize sync

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
+const sequelize = require('./db');
 const app = require('./app');
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+(async () => {
+  const port = process.env.PORT || 8080;
+  await sequelize.sync();
+  app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+  });
+})();
