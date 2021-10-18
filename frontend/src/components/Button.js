85b507747355b382df9541445426b421ec4977ad
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-10-18 03:05:22 -0300

    Add forwardRef

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,14 +1,15 @@
-export default function Button({ children, className, onClick, disabled, color = 'bg-indigo-500', type = 'button' }) {
-  return (
-    <button
-      className={`${color} border-rounded text-white font-medium py-2 px-4 disabled:opacity-60 disabled:cursor-default ${
-        className || ''
-      }`}
-      onClick={onClick}
-      disabled={disabled}
-      type={type}
-    >
-      {children}
-    </button>
-  );
-}
+import React from 'react';
+
+export default React.forwardRef(({ children, className, onClick, disabled, color = 'bg-indigo-500', type }, ref) => (
+  <button
+    className={`${color} border-rounded text-white font-medium py-2 px-4 disabled:opacity-60 disabled:cursor-default ${
+      className || ''
+    }`}
+    onClick={onClick}
+    disabled={disabled}
+    type={type}
+    ref={ref}
+  >
+    {children}
+  </button>
+));
