861663651dde6782a579de4b286eea9f1f89cf23
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-10-18 01:56:14 -0300

    Add list change event and delete action

--- frontend/src/components/TransactionList.js
+++ frontend/src/components/TransactionList.js
@@ -1,13 +1,34 @@
+import axios from 'axios';
 import { useState } from 'react';
+import useAuth from '../hooks/useAuth';
 import TransactionModal from './TransactionModal';
 
-export default function TransactionList({ list }) {
+export default function TransactionList({ list, onListChange }) {
   const [isModalShowing, setIsModalShowing] = useState(false);
   const [currTransaction, setCurrTransaction] = useState(false);
+  const {
+    user: { token },
+  } = useAuth();
 
   const handleEdit = (transaction) => {
     setCurrTransaction(transaction);
     setIsModalShowing(true);
+  };
+
+  const handleDelete = async (transaction) => {
+    try {
+      await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/transactions/${transaction.id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (typeof onListChange === 'function') onListChange('delete', transaction);
+    } catch {}
+  };
+
+  const handleModalClose = (transaction) => {
+    setIsModalShowing(false);
+    if (transaction) {
+      if (typeof onListChange === 'function') onListChange('update', transaction);
+    }
   };
   return (
     <>
@@ -56,18 +77,32 @@ export default function TransactionList({ list }) {
                     />
                   </svg>
                 </button>
+                <button
+                  className="text-indigo-500 hover:text-indigo-600 ml-2"
+                  onClick={() => handleDelete(transaction)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                    />
+                  </svg>
+                </button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
 
-      <TransactionModal
-        show={isModalShowing}
-        mode="editing"
-        onClose={() => setIsModalShowing(false)}
-        transaction={currTransaction}
-      />
+      <TransactionModal show={isModalShowing} mode="editing" onClose={handleModalClose} transaction={currTransaction} />
     </>
   );
 }
