3c9fa9d3829ea7045e86127e037479e3ca44b3b7
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-10-17 23:43:14 -0300

    Chane button type and add button colors

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,11 +1,12 @@
-export default function Button({ children, className, onClick, disabled }) {
+export default function Button({ children, className, onClick, disabled, color = 'bg-indigo-500' }) {
   return (
     <button
-      className={`bg-indigo-500 border-rounded text-white font-medium py-2 disabled:bg-indigo-400 disabled:cursor-default ${
+      className={`${color} border-rounded text-white font-medium py-2 px-4 disabled:bg-indigo-400 disabled:cursor-default ${
         className || ''
       }`}
       onClick={onClick}
       disabled={disabled}
+      type="button"
     >
       {children}
     </button>
